afef6f2b23daf36e9e03b32f2edae20673fb582b
Greih Murray <murrayg0216@outlook.com>
2023-02-06 13:32:17 -0600

    Added I2C Configuration
    Added config files for i2c, pwm, softpwm, and spi. Added code for
    i2cConfiguration.java

---
+++ micronautpi4j-utils/src/main/java/com/opensourcewithslu/utilities/PwmConfiguration.java
@@ -0,0 +1,7 @@
+package com.opensourcewithslu.utilities;
+
+import io.micronaut.context.annotation.EachProperty;
+
+@EachProperty("pi4j.pwm")
+public class PwmConfiguration {
+}
---
+++ micronautpi4j-utils/src/main/java/com/opensourcewithslu/utilities/SoftPwmConfiguration.java
@@ -0,0 +1,7 @@
+package com.opensourcewithslu.utilities;
+
+import io.micronaut.context.annotation.EachProperty;
+
+@EachProperty("pi4j.softpwm")
+public class SoftPwmConfiguration {
+}
