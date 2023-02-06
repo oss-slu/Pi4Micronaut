de97ce3c4a3ca8b1c48804bd9d71c2b310970e8b
Greih Murray <murrayg0216@outlook.com>
2023-02-06 14:04:11 -0600

    Updating Factory
    Updated factory to include spi, pwm, and i2c. Currently leaving out
    softpwm as this may be better handled elsewhere, potentially outside of
    the library.

--- micronautpi4j-utils/src/main/java/com/opensourcewithslu/utilities/Pi4JFactory.java
+++ micronautpi4j-utils/src/main/java/com/opensourcewithslu/utilities/Pi4JFactory.java
@@ -4,6 +4,12 @@ import com.pi4j.Pi4J;
 import com.pi4j.context.Context;
 import com.pi4j.io.gpio.digital.DigitalInput;
 import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.i2c.I2C;
+import com.pi4j.io.i2c.I2CConfig;
+import com.pi4j.io.pwm.Pwm;
+import com.pi4j.io.pwm.PwmConfig;
+import com.pi4j.io.spi.Spi;
+import com.pi4j.io.spi.SpiConfig;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.EachBean;
 import io.micronaut.context.annotation.Factory;
@@ -39,5 +45,40 @@ public class Pi4JFactory {
                 .pull(config.getPull())
                 .provider(config.getProvider());
         return pi4jContext.create(inputConfigBuilder);
+    }
+
+    @EachBean(PwmConfiguration.class)
+    public PwmConfig createPwm(PwmConfiguration config, Context pi4jContext) {
+        var outputConfigBuilder = Pwm.newConfigBuilder(pi4jContext)
+                .id(config.getId())
+                .name(config.getName())
+                .address(config.getAddress())
+                .frequency(config.getFrequency())
+                .initial(config.getInital())
+                .shutdown(config.getShutdown())
+                .build();
+        return outputConfigBuilder;
+    }
+
+    @EachBean(SpiConfiguration.class)
+    public SpiConfig createDigitalOutput(SpiConfiguration config, Context pi4jContext) {
+        var outputConfigBuilder = Spi.newConfigBuilder(pi4jContext)
+                .id(config.getId())
+                .channel(config.getChannel())
+                .mode(config.getMode())
+                .baud(config.getBaud())
+                .build();
+        return outputConfigBuilder;
+    }
+
+    @EachBean(i2cConfiguration.class)
+    public I2CConfig createDigitalOutput(i2cConfiguration config, Context pi4jContext) {
+        var outputConfigBuilder = I2C.newConfigBuilder(pi4jContext)
+                .id(config.getId())
+                .name(config.getName())
+                .bus(config.getBus())
+                .device(config.getDevice())
+                .build();
+        return outputConfigBuilder;
     }
 }
