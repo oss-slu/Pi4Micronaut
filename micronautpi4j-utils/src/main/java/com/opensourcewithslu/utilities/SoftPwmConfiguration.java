abc3790d6b2a1814e4d374c3c7494b6f1071fa54
Greih Murray <murrayg0216@outlook.com>
2023-02-06 13:47:56 -0600

    SoftPWM Config code
    Added code for SoftPwmConfiguration

--- micronautpi4j-utils/src/main/java/com/opensourcewithslu/utilities/SoftPwmConfiguration.java
+++ micronautpi4j-utils/src/main/java/com/opensourcewithslu/utilities/SoftPwmConfiguration.java
@@ -1,7 +1,47 @@
 package com.opensourcewithslu.utilities;
 
 import io.micronaut.context.annotation.EachProperty;
+import io.micronaut.context.annotation.Parameter;
 
 @EachProperty("pi4j.softpwm")
 public class SoftPwmConfiguration {
+    private final String id;
+
+    private int pin;
+
+    private int value;
+
+    private int range;
+
+    public SoftPwmConfiguration(@Parameter String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
 }
