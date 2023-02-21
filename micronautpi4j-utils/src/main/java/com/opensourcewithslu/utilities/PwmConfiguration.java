160ed3a313405d2b2e366f5a2d726dd0b63ee6e0
Greih Murray <murrayg0216@outlook.com>
2023-02-21 14:28:35 -0600

    Modifying PWM
    Modified PWM config and pwm in factory to better fit with pi4j

--- micronautpi4j-utils/src/main/java/com/opensourcewithslu/utilities/PwmConfiguration.java
+++ micronautpi4j-utils/src/main/java/com/opensourcewithslu/utilities/PwmConfiguration.java
@@ -1,20 +1,17 @@
 package com.opensourcewithslu.utilities;
 
+import com.pi4j.io.pwm.PwmType;
 import io.micronaut.context.annotation.EachProperty;
 import io.micronaut.context.annotation.Parameter;
 
 @EachProperty("pi4j.pwm")
 public class PwmConfiguration {
     private final String id;
-
     private String name;
-
     private int address;
-
-    private int frequency;
-
+    private PwmType pwmType;
+    private String provider;
     private int inital;
-
     private int shutdown;
 
     public PwmConfiguration(@Parameter String id) {
@@ -41,14 +38,6 @@ public class PwmConfiguration {
         this.address = address;
     }
 
-    public int getFrequency() {
-        return frequency;
-    }
-
-    public void setFrequency(int frequency) {
-        this.frequency = frequency;
-    }
-
     public int getInital() {
         return inital;
     }
@@ -63,5 +52,21 @@ public class PwmConfiguration {
 
     public void setShutdown(int shutdown) {
         this.shutdown = shutdown;
+    }
+
+    public PwmType getPwmType() {
+        return pwmType;
+    }
+
+    public void setPwmType(PwmType pwmType) {
+        this.pwmType = pwmType;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
     }
 }
