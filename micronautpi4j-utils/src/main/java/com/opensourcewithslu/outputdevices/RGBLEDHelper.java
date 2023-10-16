97e7551ba2c3a4d34ec988decb5b1a47138431fa
ruthvikm <116761292+ruthvikm@users.noreply.github.com>
2023-10-16 13:52:38 -0500

    Merging develop into main (#87)
    * Completes #47 (#73)
    
    * allow setting led color via hex value
    
    * set controller endpoint for setting RGB via hex
    
    * fixed spacing error in url for hex converter with frequencies
    
    ---------
    
    Co-authored-by: Adrian Swindle <adrian.swindle@slu.edu>
    
    * Added documentation for RGB LED circuit guide. Issue 74 (#79)
    
    * Added location for RGB LED circuit guide.
    
    * Added diagrams and link for hardware setup
    
    * Addresses requested changes
    
    * Changed default frequency to 200
    
    ---------
    
    Co-authored-by: ruthvikm <ruthviknathchowdary@gmail.com>
    
    ---------
    
    Co-authored-by: jyanev <74617990+jyanev@users.noreply.github.com>
    Co-authored-by: Adrian Swindle <adrian.swindle@slu.edu>
    Co-authored-by: Joe Folen <96354144+joefol@users.noreply.github.com>

--- micronautpi4j-utils/src/main/java/com/opensourcewithslu/outputdevices/RGBLEDHelper.java
+++ micronautpi4j-utils/src/main/java/com/opensourcewithslu/outputdevices/RGBLEDHelper.java
@@ -30,9 +30,9 @@ public class RGBLEDHelper {
     public void setColor(int[] colors)
     //end::method[]
     {
-        red.on(colors[0], 50);
-        green.on(colors[1], 50);
-        blue.on(colors[2], 50);
+        red.on(colors[0], 200);
+        green.on(colors[1], 200);
+        blue.on(colors[2], 200);
     }
 
     //tag::method[]
@@ -45,14 +45,34 @@ public class RGBLEDHelper {
         blue.on(colors[2], frequency[2]);
     }
 
-    public void setColorHex(String hex) {
+    //tag::method[]
+    public void setColorHex(String hex)
+    //end::method[]
+    {
         log.trace("setting the color via hex");
-        //input the hex splitting and frequency stuff
-    }
+        // hex splitting into rbg int values
+        int r = (Integer.decode(hex) & 0xFF0000) >> 16;
+        int g = (Integer.decode(hex) & 0xFF00) >> 8;
+        int b = (Integer.decode(hex) & 0xFF);
 
-    public void setColorHex(String hex, int frequency) {
+        // no frequency input, default value 200
+        red.on(r, 200);
+        green.on(g, 200);
+        blue.on(b, 200);
+    }
+    //tag::method[]
+    public void setColorHex(String hex, int[] frequency)
+    //end::method[]
+    {
         log.trace("setting the color and frequency via hex and int");
-        // input the hex splitting and frequency stuff
+        // hex splitting into rbg int values
+        int r = (Integer.decode(hex) & 0xFF0000) >> 16;
+        int g = (Integer.decode(hex) & 0xFF00) >> 8;
+        int b = (Integer.decode(hex) & 0xFF);
+
+        red.on(r, frequency[0]);
+        green.on(g, frequency[1]);
+        blue.on(b, frequency[2]);
     }
 
     //tag::method[]
@@ -60,7 +80,7 @@ public class RGBLEDHelper {
     //end::method[]
     {
         log.trace("Set red");
-        this.red.on(red, 50);
+        this.red.on(red, 200);
     }
 
     //tag::method[]
@@ -76,7 +96,7 @@ public class RGBLEDHelper {
     //end::method[]
     {
         log.trace("set blue");
-        this.blue.on(blue, 50);
+        this.blue.on(blue, 200);
     }
 
     //tag::method[]
@@ -92,7 +112,7 @@ public class RGBLEDHelper {
     //end::method[]
     {
         log.trace("set green");
-        this.green.on(green, 50);
+        this.green.on(green, 200);
     }
 
     //tag::method[]
@@ -117,8 +137,8 @@ public class RGBLEDHelper {
     //end::method[]
     {
         log.trace("turning on each LED pin and setting to 100");
-        this.red.on(100, 50);
-        this.green.on(100, 50);
-        this.blue.on(100, 50);
+        this.red.on(100, 200);
+        this.green.on(100, 200);
+        this.blue.on(100, 200);
     }
 }
