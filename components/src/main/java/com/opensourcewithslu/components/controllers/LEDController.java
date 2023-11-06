c3e685b78c83983325e807a745a9dd2a36edf45e
Alex Delgado <alexander.delgado@slu.edu>
2023-11-06 12:38:10 -0600

    Provided the requested changes: bullet points, omegas, and corrected image path. Added a second method to the LEDController to take care of Led2. Added LED_circuit.png to the img file.

--- components/src/main/java/com/opensourcewithslu/components/controllers/LEDController.java
+++ components/src/main/java/com/opensourcewithslu/components/controllers/LEDController.java
@@ -10,18 +10,24 @@ import jakarta.inject.Named;
 @Controller("/led")
 public class LEDController {
     private final LEDHelper ledHelper;
+    private final LEDHelper ledHelper2;
 
-    public LEDController(@Named("Led")DigitalOutput led){
+    public LEDController(@Named("Led")DigitalOutput led, @Named("Led2")DigitalOutput led2){
         this.ledHelper = new LEDHelper(led);
+        this.ledHelper2 = new LEDHelper(led2);
     }
 
-    @Get("/led")
-    public void checkLed(){
+    @Get("/led1")
+    public void checkLed1(){
         /*deviceOff method turns on the LED after checking the output. This
         will turn on the LED
         */
 
         ledHelper.deviceOff();
+    }
+    @Get("/led2")
+    public void checkLed2(){
+        ledHelper2.deviceOff();
     }
 
 }
