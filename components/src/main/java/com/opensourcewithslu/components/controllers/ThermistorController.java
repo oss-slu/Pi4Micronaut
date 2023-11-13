9f989168fb2c0b6e8a0dcff56f5a286bd2ead0ad
Joe Folen <joe.folen@gmail.com>
2023-11-13 12:23:28 -0600

    Beginning thermistor support. Issue 39.

---
+++ components/src/main/java/com/opensourcewithslu/components/controllers/ThermistorController.java
@@ -0,0 +1,9 @@
+package com.opensourcewithslu.components.controllers;
+
+import io.micronaut.http.annotation.Controller;
+
+@Controller("/thermistor")
+public class ThermistorController {
+
+    
+}
---
+++ micronautpi4j-utils/src/main/java/com/opensourcewithslu/inputdevices/ThermistorHelper.java
@@ -0,0 +1,6 @@
+package com.opensourcewithslu.inputdevices;
+
+public class ThermistorHelper {
+
+
+}
