d3c6f27ec417a2f8c4de5e1f52923a24ae92005e
Joe Folen <joe.folen@gmail.com>
2023-11-13 12:45:55 -0600

    yml changes

--- micronautpi4j-utils/src/main/java/com/opensourcewithslu/inputdevices/ThermistorHelper.java
+++ micronautpi4j-utils/src/main/java/com/opensourcewithslu/inputdevices/ThermistorHelper.java
@@ -1,6 +1,10 @@
 package com.opensourcewithslu.inputdevices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ThermistorHelper {
+    private static final Logger log = LoggerFactory.getLogger(ThermistorHelper.class);
 
 
 }
