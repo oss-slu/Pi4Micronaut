94fbf82df854ecc0b73ea006111d936106237a5d
Joe Folen <joe.folen@gmail.com>
2023-11-17 14:27:52 -0600

    updates

--- components/src/main/java/com/opensourcewithslu/components/controllers/ThermistorController.java
+++ components/src/main/java/com/opensourcewithslu/components/controllers/ThermistorController.java
@@ -1,9 +1,23 @@
 package com.opensourcewithslu.components.controllers;
 
+import com.opensourcewithslu.inputdevices.ThermistorHelper;
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalOutput;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import jakarta.inject.Named;
 
 @Controller("/thermistor")
 public class ThermistorController {
 
-    
+    private final ThermistorHelper thermistorHelper;
+    //maybe use multipin
+    public ThermistorController(@Named("thermistor-input") DigitalInput sensorInput, @Named("thermistor-output") DigitalOutput sensorOutput){
+        this.thermistorHelper = new ThermistorHelper(sensorInput, sensorOutput);
+    }
+
+    @Get("/value")
+    public int getThermistorValue(){
+        return thermistorHelper.getThermistorValue();
+    }
 }
--- micronautpi4j-utils/src/main/java/com/opensourcewithslu/inputdevices/ThermistorHelper.java
+++ micronautpi4j-utils/src/main/java/com/opensourcewithslu/inputdevices/ThermistorHelper.java
@@ -1,10 +1,20 @@
 package com.opensourcewithslu.inputdevices;
 
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalOutput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ThermistorHelper {
     private static final Logger log = LoggerFactory.getLogger(ThermistorHelper.class);
+    private String helperName;
+    private int globalValue;
 
+    public ThermistorHelper(DigitalInput sensorInput, DigitalOutput sensorOutput){
+        //maybe use multipin
+    }
 
+    public int getThermistorValue() {
+        return 1;
+    }
 }
