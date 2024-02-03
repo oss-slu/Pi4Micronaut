43db4cf13a06b9d33f22b6a3d8f3f766a1e7668f
Adrian Swindle <aswindle724@gmail.com>
2024-02-03 14:34:12 -0600

    javadoc comments added to the utility files

--- micronautpi4j-utils/src/main/java/com/opensourcewithslu/utilities/MultipinConfiguration.java
+++ micronautpi4j-utils/src/main/java/com/opensourcewithslu/utilities/MultipinConfiguration.java
@@ -9,20 +9,36 @@ import io.micronaut.context.annotation.Prototype;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Class for configuring multipin components.
+ */
 @Prototype
 public class MultipinConfiguration {
     private final String id;
     private final Object[] components;
 
+    /**
+     *
+     * @param id The configuration id as defined in the application.yml
+     * @param components The array of components that are a part of the multipin component.
+     */
     public MultipinConfiguration(String id, Object[] components){
         this.id = id;
         this.components = components;
     }
 
+    /**
+     * Gets the id of the component.
+     * @return The id of the component.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets the components that are part of the overall component.
+     * @return an array of component objects.
+     */
     public Object[] getComponents() {
         return components;
     }
