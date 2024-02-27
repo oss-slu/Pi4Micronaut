7ff4845967d0adc8b03f998fb3f92681279cf74b
Joe Folen <joe.folen@gmail.com>
2024-02-26 23:12:10 -0600

    PIR sensor helper and controller updates

--- components/src/main/java/com/opensourcewithslu/components/controllers/PIRSensorController.java
+++ components/src/main/java/com/opensourcewithslu/components/controllers/PIRSensorController.java
@@ -5,6 +5,7 @@ import com.opensourcewithslu.outputdevices.RGBLEDHelper;
 import com.opensourcewithslu.utilities.MultipinConfiguration;
 import com.pi4j.io.gpio.digital.DigitalOutput;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
 import jakarta.inject.Named;
 
 @Controller("/pirSensor")
@@ -18,6 +19,28 @@ public class PIRSensorController {
                                @Named("rgb-led-2")MultipinConfiguration rgbled) {
         this.pirSensorHelper = new PIRSensorHelper(pirSensor);
         this.rgbledHelper = new RGBLEDHelper(rgbled);
+    }
+
+    @Get("/enable")
+    public void enablePIRSensor() {
+
+        int[] yellow = {255,255,0};
+        int[] blue = {0,0,255};
+
+        pirSensorHelper.addEventListener(e -> {
+            if (pirSensorHelper.isMoving) {
+                rgbledHelper.setColor(yellow);
+            }
+            else {
+                rgbledHelper.setColor(blue);
+            }
+        });
+    }
+
+    @Get("/disable")
+    public void disablePIRSensor() {
+        pirSensorHelper.removeEventListener();
+        rgbledHelper.ledOff();
     }
 
 }
--- pi4micronaut-utils/src/main/java/com/opensourcewithslu/outputdevices/PIRSensorHelper.java
+++ pi4micronaut-utils/src/main/java/com/opensourcewithslu/outputdevices/PIRSensorHelper.java
@@ -1,6 +1,7 @@
 package com.opensourcewithslu.outputdevices;
 
 import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalStateChangeListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,14 +9,43 @@ public class PIRSensorHelper {
 
     private static final Logger log = LoggerFactory.getLogger(PIRSensorHelper.class);
 
-    private final DigitalOutput pirSensorInput;
+    private final DigitalOutput pirSensorOutput;
+
+    private DigitalStateChangeListener pirSensorOutputListener;
 
     public boolean isMoving;
 
-    public PIRSensorHelper(DigitalOutput pirSensorInput)
+    public PIRSensorHelper(DigitalOutput pirSensorOutput)
     {
-        this.pirSensorInput = pirSensorInput;
+        this.pirSensorOutput = pirSensorOutput;
+        this.isMoving = pirSensorOutput.isHigh();
+
+        initialize();
     }
 
+    public void initialize()
+    {
+        log.info("Initializing PIR Sensor");
 
+        pirSensorOutputListener = e-> isMoving = pirSensorOutput.isHigh();
+        pirSensorOutput.addListener(pirSensorOutputListener);
+    }
+
+    public void addEventListener(DigitalStateChangeListener function)
+    {
+        log.info("Adding event listener");
+
+        pirSensorOutputListener = function;
+        pirSensorOutput.addListener(pirSensorOutputListener);
+    }
+
+    public void removeEventListener()
+    {
+        log.info("Removing event listener");
+
+        if (pirSensorOutputListener != null) {
+            pirSensorOutput.removeListener(pirSensorOutputListener);
+            pirSensorOutputListener = null;
+        }
+    }
 }
