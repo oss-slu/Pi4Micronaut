163d31d562f1e400f3b65b33092bcf4b501b9980
Adrian Swindle <aswindle724@gmail.com>
2024-03-09 20:20:44 -0600

    32 bit note in asciidoc, new methods and controller stuff for lcd

--- pi4micronaut-utils/src/main/java/com/opensourcewithslu/outputdevices/LCD1602Helper.java
+++ pi4micronaut-utils/src/main/java/com/opensourcewithslu/outputdevices/LCD1602Helper.java
@@ -24,7 +24,7 @@ public class LCD1602Helper {
     public LCD1602Helper(I2CConfig i2CConfig, Context pi4jContext)
     //end::const[]
     {
-        
+
         this.lcdDisplay = new LcdDisplay(pi4jContext, 2, 16, i2CConfig.bus(), i2CConfig.device());
         lcdDisplay.setDisplayBacklight(true);
         log.info("LCD is set up with 2 rows and 16 columns. Backlight is on by default");
@@ -54,6 +54,12 @@ public class LCD1602Helper {
         log.info("writing on " + line + "line");
         lcdDisplay.displayText(text, line);
     }
+
+
+    public void writeCharacter(char charvalue){
+        lcdDisplay.writeCharacter(charvalue);
+    }
+
 
     /**
      * Setting the backlight state of the LCD based off the boolean input. If true, then the backlight is set as on.
@@ -96,4 +102,94 @@ public class LCD1602Helper {
     {
         lcdDisplay.clearLine(line);
     }
+
+    /**
+     * Turns off LCD Display. Works the same as backlight off, but a write or clear command will turn the display back on.
+     */
+    public void turnOff(){
+        lcdDisplay.off();
+    }
+
+    /// Below methods do not work
+
+    /**
+     * Moves the cursor to
+     */
+    public void moveCursorHome(){
+        lcdDisplay.moveCursorHome();
+    }
+
+    /**
+     * Moves cursor to the left.
+     */
+    public void moveCursorLeft(){
+        lcdDisplay.moveCursorLeft();
+    }
+
+    /**
+     * Moves crusor to the right.
+     */
+    public void moveCursorRight(){
+        lcdDisplay.moveCursorRight();
+    }
+
+    /**
+     * Moves the display left
+     */
+    public void moveDisplayLeft(){
+        lcdDisplay.moveDisplayLeft();
+    }
+
+    /**
+     * Move the display right
+     */
+    public void moveDisplayRight(){
+        lcdDisplay.moveDisplayRight();
+    }
+
+    /**
+     * Sets the cursor to blink
+     * @param blink Set True to set the cursor to blink.
+     */
+    public void setCursorBlinking(boolean blink){
+        lcdDisplay.setCursorBlinking(blink);
+    }
+
+    /**
+     * Moves the cursor to the inputted display
+     * @param line The line to move the cursor to.
+     */
+    public void setCursorToLine(int line){
+        lcdDisplay.setCursorToLine(line);
+    }
+
+    /**
+     * Sets the cursor to the position and line specified.
+     * @param digit The position starting at 1 at the left most position. 16 is the right most position.
+     * @param line The line to move the cursor to.
+     */
+    public void setCursorToPosition(int digit, int line){
+        lcdDisplay.setCursorToPosition(digit,line);
+    }
+
+    /**
+     * Sets the cursor to show or not show
+     * @param show True to set the cursor to visible
+     */
+    public void setCursorVisibility(boolean show){
+        lcdDisplay.setCursorVisibility(show);
+    }
+
+//    /**
+//     * Creates a TBD
+//     * @param location The position on the current line to place the character.
+//     * @param character The character represented by a byte array.
+//     */
+//    public void createCharacter(int location,byte [] character){
+//        lcdDisplay.createCharacter(location,character);
+//    }
+
+
+
+
 }
