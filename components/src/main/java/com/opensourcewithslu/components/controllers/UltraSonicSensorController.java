d70685b014844210193c2a28061522906b7531ba
jyanev <jyanev1@gmail.com>
2024-03-18 13:05:05 -0500

    added documentation for UltraSonic Sensor

--- components/src/main/java/com/opensourcewithslu/components/controllers/UltraSonicSensorController.java
+++ components/src/main/java/com/opensourcewithslu/components/controllers/UltraSonicSensorController.java
@@ -8,7 +8,7 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import jakarta.inject.Named;
 
-
+//tag::ex[]
 @Controller("/ultraSound")
 public class UltraSonicSensorController {
 
@@ -55,3 +55,4 @@ public class UltraSonicSensorController {
         return "Ultra Sonic Sensor Disabled";
     }
 }
+//end::ex[]
