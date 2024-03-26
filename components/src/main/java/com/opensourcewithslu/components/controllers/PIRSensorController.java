b54f410f9589db4494304eb80d2837da6fb307ff
Adrian Swindle <aswindle724@gmail.com>
2024-03-25 19:48:39 -0500

    pir sensor documentation

--- components/src/main/java/com/opensourcewithslu/components/controllers/PIRSensorController.java
+++ components/src/main/java/com/opensourcewithslu/components/controllers/PIRSensorController.java
@@ -11,33 +11,28 @@ import jakarta.inject.Named;
 /**
  * The PIRSensorController class is used with the PIRSensorHelper class and RGBHelper class to implement a PIR motion sensor with an RGB LED light.
  */
+
+//tag::ex[]
 @Controller("/pirSensor")
 public class PIRSensorController {
-
     private final PIRSensorHelper pirSensorHelper;
-
     private final RGBLEDHelper rgbledHelper;
-
     /**
      * The PirSensorController constructor.
      * @param pirSensor A Pi4J DigitalInput object.
      * @param rgbLed A MultiPinConfiguration object.
      */
-    public PIRSensorController(@Named("pir-sensor")DigitalInput pirSensor,
-                               @Named("rgb-led-2") MultiPinConfiguration rgbLed) {
+    public PIRSensorController(@Named("pir-sensor")DigitalInput pirSensor,@Named("rgb-led-2") MultiPinConfiguration rgbLed) {
         this.pirSensorHelper = new PIRSensorHelper(pirSensor);
         this.rgbledHelper = new RGBLEDHelper(rgbLed);
     }
-
     /**
      * Enables the PIR sensor by adding an event listener which sets the RGB LED to red when movement is detected and green otherwise.
      */
     @Get("/enable")
     public void enablePIRSensor() {
-
         int[] red = {255,0,0};
         int[] green = {0,255,0};
-
         pirSensorHelper.addEventListener(e -> {
             if (pirSensorHelper.isMoving) {
                 rgbledHelper.setColor(red);
@@ -47,7 +42,6 @@ public class PIRSensorController {
             }
         });
     }
-
     /**
      * Disables the controller by removing the event listener and turning off the RGB LED.
      */
@@ -56,5 +50,5 @@ public class PIRSensorController {
         pirSensorHelper.removeEventListener();
         rgbledHelper.ledOff();
     }
-
 }
+//end::ex[]
