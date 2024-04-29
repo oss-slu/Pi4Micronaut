943ba67d2f7b8cfb201000bca46ea159cc0682f1
Adrian Swindle <adrian.swindle@slu.edu>
2024-04-29 11:53:15 -0500

    draft of micro switch helper, controller and application.yml

---
+++ components/src/main/java/com/opensourcewithslu/components/controllers/MicroSwitchController.java
@@ -0,0 +1,47 @@
+package com.opensourcewithslu.components.controllers;
+
+//import com.opensourcewithslu.inputdevices.MicroSwitchHelper;
+//import com.opensourcewithslu.outputdevices.LEDHelper;
+//import com.pi4j.io.gpio.digital.DigitalInput;
+//import com.pi4j.io.gpio.digital.DigitalOutput;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import jakarta.inject.Named;
+
+//tag::ex[]
+@Controller("/microSwitch")
+public class MicroSwitchController {
+
+    private final MicroSwitchHelper microSwitchHelper;
+
+
+
+    public MicroSwitchController(@Named("micro-switch") DigitalInput microSwitch,
+                                 @Named("led1") DigitalOutput led1,
+                                 @Named("led2") DigitalOutput led2) {
+        this.microSwitchHelper = new MicroSwitchHelper(microSwitch);
+        this.ledHelper1 = new LEDHelper(led1);
+        this.ledHelper2 = new LEDHelper(led2);
+    }
+
+    @Get("/enable")
+    public void enableMicroSwitch() {
+        microSwitchHelper.addEventListener(e -> {
+            if (microSwitchHelper.isPressed) {
+                ledHelper1.ledOff();
+                ledHelper2.ledOn();
+
+            }
+            else {
+                ledHelper1.ledOn();
+                ledHelper2.ledOff();
+            }
+        });
+    }
+
+    @Get("/disable")
+    public void disableMicroSwitch() {
+        microSwitchHelper.removeEventListener();
+    }
+}
+//end::ex[]
