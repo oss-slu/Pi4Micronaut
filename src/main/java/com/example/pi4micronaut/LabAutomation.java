2a75b4edd842f84d3fbd98993883bfa6b1c0f744
jyanev <jyanev1@gmail.com>
2024-05-03 15:30:32 -0500

    implemented Mouse Lab Automation System

--- src/main/java/com/example/pi4micronaut/LabAutomation.java
+++ src/main/java/com/example/pi4micronaut/LabAutomation.java
@@ -2,35 +2,72 @@ package com.example.pi4micronaut;
 
 import com.opensourcewithslu.inputdevices.*;
 import com.opensourcewithslu.outputdevices.*;
+import com.opensourcewithslu.utilities.MultipinConfiguration;
+
 
 import com.pi4j.context.Context;
 
 import com.pi4j.io.gpio.digital.DigitalInput;
-import com.pi4j.io.gpio.digital.DigitalOutput;
 import com.pi4j.io.i2c.I2CConfig;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
 import jakarta.inject.Named;
+
+import java.lang.Thread;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/LabAutomation")
 public class LabAutomation {
     private final LCD1602Helper lcd;
-    private final LEDHelper led;
-    private final UltraSonicSensorHelper ultraSonicSensorStart;
-    private final UltraSonicSensorHelper ultraSonicSensorEnd;
+    private final RGBLEDHelper rgb;
+    private final PIRSensorHelper pirStart;
+    private final PIRSensorHelper pirEnd;
+
+    private Thread timer;
+    private boolean inMaze;
+    private int timeInMaze;
+    private long startTime;
+    private long endTime;
+
+    private static final Logger log = LoggerFactory.getLogger(LabAutomation.class);
 
     public LabAutomation(
             @Named("lcd") I2CConfig lcd,
-            @Named("led") DigitalOutput led,
-            @Named("ultra-sonic-trig1") DigitalOutput trig1,
-            @Named("ultra-sonic-echo1") DigitalInput echo1,
-            @Named("ultra-sonic-trig2") DigitalOutput trig2,
-            @Named("ultra-sonic-echo2") DigitalInput echo2,
+            @Named("rgb-led") MultipinConfiguration rgb,
+            @Named("pir-sensor-1") DigitalInput pirStart,
+            @Named("pir-sensor-2") DigitalInput pirEnd,
             Context pi4j)
     {
         this.lcd = new LCD1602Helper(lcd,pi4j);
-        this.led = new LEDHelper(led);
-        this.ultraSonicSensorStart = new UltraSonicSensorHelper(trig1,echo1);
-        this.ultraSonicSensorEnd = new UltraSonicSensorHelper(trig2,echo2);
+        this.rgb = new RGBLEDHelper(rgb);
+        this.pirStart = new PIRSensorHelper(pirStart);
+        this.pirEnd = new PIRSensorHelper(pirEnd);
+
+        this.inMaze = false;
+        this.timeInMaze = 0;
+
+        this.timer = new Thread(() -> {
+
+            while ( true ) {
+
+                try { Thread.sleep(50); }
+                catch (InterruptedException e) { log.error("cant sleep :(", e); }
+
+                if (!this.inMaze ) { continue; }
+
+                this.lcd.writeTextAtLine("Current Time:", 1);
+                this.lcd.writeTextAtLine(String.format("%d seconds", this.timeInMaze), 2);
+                this.timeInMaze++;
+                this.log.info("Mouse in Maze");
+
+                try { Thread.sleep(950); }
+                catch (InterruptedException e) { log.error("cant sleep :(", e); }
+            }
+        });
+        this.timer.start();
+
     }
 
     /*
@@ -42,4 +79,32 @@ public class LabAutomation {
         - Use LCD to display messages like Mouse entered, mouse left and total time
      */
 
+    @Get("/enable")
+    public void enable() {
+        this.pirStart.addEventListener((event) -> {
+
+            if ( this.pirStart.isMoving  && !this.inMaze ) {
+                this.timeInMaze = 0;
+                this.inMaze = true;
+                this.rgb.setGreen(255);
+                this.rgb.setRed(0);
+                this.startTime = System.currentTimeMillis();
+                this.lcd.clearDisplay();
+            }
+
+        });
+
+        this.pirEnd.addEventListener((event) -> {
+
+            if ( this.pirEnd.isMoving && this.inMaze ) {
+                this.inMaze = false;
+                this.rgb.setGreen(0);
+                this.rgb.setRed(255);
+                this.endTime = System.currentTimeMillis();
+                long finishTime = ( this.endTime - this.startTime ) / 1000;
+                this.lcd.writeTextAtLine("Final Time:", 1);
+                this.lcd.writeTextAtLine(String.format("%d seconds", finishTime), 2);
+            }
+        });
+    }
 }
