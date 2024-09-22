7c9ea2f8350a9f404776eb068837adc5c87f1c46
Thomas Macas <t33mac@comcast.net>
2024-09-22 10:38:44 -0500

    Resolving White Spaces

--- components/src/test/java/com/opensourcewithslu/components/ComponentsTest.java
+++ components/src/test/java/com/opensourcewithslu/components/ComponentsTest.java
@@ -4,7 +4,6 @@ import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-
 import jakarta.inject.Inject;
 
 @MicronautTest
