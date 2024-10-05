dd5f12217ba87fbf07775cd0b369b833f9ca7d93
Leandru Martin <leandru.martin@slu.edu>
2024-10-05 10:10:32 -0500

    Renamed more variables and comments from "DC motor" to simply "motor", fixed bug with motor speed checking

--- pi4micronaut-utils/src/main/java/com/opensourcewithslu/outputdevices/MotorHelper.java
+++ pi4micronaut-utils/src/main/java/com/opensourcewithslu/outputdevices/MotorHelper.java
@@ -7,56 +7,56 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Helper class to control a DC motor using PWM (Pulse Width Modulation).
- * This class provides methods to enable, disable, and set the speed of a DC motor.
+ * This class provides methods to enable, disable, and set the speed of a motor.
  */
 public class MotorHelper {
 
     private static final Logger log = LoggerFactory.getLogger(MotorHelper.class);
-    private static final int FREQUENCY = 50; // Frequency for PWM signal in Hz, typical for DC motors.
-    private boolean isEnabled = false; // State tracking variable for the DC motor.
-    private final Pwm DCMotor; // PWM interface for the DC motor.
+    private static final int FREQUENCY = 50; // Frequency for PWM signal in Hz.
+    private boolean isEnabled = false; // State tracking variable for the motor.
+    private final Pwm motor; // PWM interface for the motor.
     private final DigitalOutput pin1; // GPIO pin 1 for motor direction.
     private final DigitalOutput pin2; // GPIO pin 2 for motor direction.
-    private boolean isClockwise = true; // Direction of the DC motor.
+    private boolean isClockwise = true; // Direction of the motor.
 
     /**
-     * Constructs a new DCMotorHelper.
+     * Constructs a new MotorHelper.
      *
-     * @param DCMotor A PWM interface to control the DC motor.
+     * @param motor A PWM interface to control the motor.
      * @param pin1 A DigitalOutput interface for the first GPIO pin.
      * @param pin2 A DigitalOutput interface for the second GPIO pin.
      */
     //tag::const[]
-    public MotorHelper(Pwm DCMotor, DigitalOutput pin1, DigitalOutput pin2)
+    public MotorHelper(Pwm motor, DigitalOutput pin1, DigitalOutput pin2)
     //end::const[]
     {
-        this.DCMotor = DCMotor;
+        this.motor = motor;
         this.pin1 = pin1;
         this.pin2 = pin2;
     }
 
     /**
      * Enables the DC motor by setting an initial duty cycle and frequency.
-     * The DC motor remains disabled until this method is called.
+     * The motor remains disabled until this method is called.
      */
     //tag::[method]
     public void enable()
     //end::[method]
     {
         log.info("Enabling DC motor");
-        DCMotor.on(0, FREQUENCY); // Initializes PWM signal with 0% duty cycle.
+        motor.on(0, FREQUENCY); // Initializes PWM signal with 0% duty cycle.
         isEnabled = true;
     }
 
     /**
-     * Disables the DC motor, effectively stopping any ongoing PWM signal.
+     * Disables the motor, effectively stopping any ongoing PWM signal.
      */
     //tag::[method]
     public void disable()
     //end::[method]
     {
         log.info("Disabling DC motor");
-        DCMotor.off(); // Stops the PWM signal.
+        motor.off(); // Stops the PWM signal.
         isEnabled = false;
     }
 
@@ -64,7 +64,7 @@ public class MotorHelper {
      * Sets the speed of the DC motor.
      * This method calculates the necessary pulse width and duty cycle to achieve the specified speed.
      *
-     * @param speed the target speed for the DC motor, as a percentage between 0 and 1.
+     * @param speed the target speed for the motor, as a percentage between 0 and 1.
      */
     //tag::[method]
     public void setSpeed(double speed)
@@ -75,20 +75,20 @@ public class MotorHelper {
             return;
         }
 
-        if (speed < 0 || speed > 1) {
-            log.info("You must enter a speed between 0 and 1.");
+        if (speed < 0 || speed > 100) {
+            log.info("You must enter a speed between 0 and 100.");
             return;
         }
 
-        log.info("Setting DC speed to {}%", speed);
+        log.info("Setting motor speed to {}%", speed);
 
-        DCMotor.on(speed, FREQUENCY);
+        motor.on(speed, FREQUENCY);
     }
 
     /**
      * Sets the direction of the DC motor.
      *
-     * @param clockwise whether the DC motor should rotate clockwise.
+     * @param clockwise whether the motor should rotate clockwise.
      */
     //tag::[method]
     public void setClockwise(boolean clockwise)
@@ -99,7 +99,7 @@ public class MotorHelper {
             return;
         }
 
-        log.info("Setting DC motor direction clockwise to {}", clockwise);
+        log.info("Setting motor direction clockwise to {}", clockwise);
         if (clockwise) {
             pin1.high();
             pin2.low();
@@ -112,7 +112,7 @@ public class MotorHelper {
     }
 
     /**
-     * Switches the direction of the DC motor.
+     * Switches the direction of the motor.
      */
     //tag::[method]
     public void switchDirection()
