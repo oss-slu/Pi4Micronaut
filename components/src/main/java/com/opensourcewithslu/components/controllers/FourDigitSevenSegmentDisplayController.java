b563b5d4e0f41e5d9b97fcd4c062c2affbc1a4c8
Leandru Martin <leandru.martin@slu.edu>
2024-10-28 09:54:19 -0500

    Added endpoint to display a number, and functionality for it using tentative implementation of single-digit seven-segment display helper.

--- components/src/main/java/com/opensourcewithslu/components/controllers/FourDigitSevenSegmentDisplayController.java
+++ components/src/main/java/com/opensourcewithslu/components/controllers/FourDigitSevenSegmentDisplayController.java
@@ -3,6 +3,7 @@ package com.opensourcewithslu.components.controllers;
 import com.opensourcewithslu.outputdevices.FourDigitSevenSegmentDisplayHelper;
 import com.opensourcewithslu.utilities.MultiPinConfiguration;
 import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.Digital;
 import com.pi4j.io.gpio.digital.DigitalOutput;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -13,8 +14,19 @@ import jakarta.inject.Named;
 public class FourDigitSevenSegmentDisplayController {
     private final FourDigitSevenSegmentDisplayHelper fourDigitSevenSegmentDisplayHelper;
 
-    public FourDigitSevenSegmentDisplayController(@Named("4digit7segment") MultiPinConfiguration fourdigsevenseg) {
-        this.fourDigitSevenSegmentDisplayHelper = new FourDigitSevenSegmentDisplayHelper(fourdigsevenseg);
+    public FourDigitSevenSegmentDisplayController(@Named("4digit7segment") MultiPinConfiguration fourdigsevenseg,
+                                                  @Named("display1") DigitalOutput display1,
+                                                  @Named("display1") DigitalOutput display2,
+                                                  @Named("display1") DigitalOutput display3,
+                                                  @Named("display1") DigitalOutput display4
+    ) {
+        DigitalOutput[] displayList = {display1, display2, display3, display4};
+        this.fourDigitSevenSegmentDisplayHelper = new FourDigitSevenSegmentDisplayHelper(fourdigsevenseg, displayList);
+    }
+
+    @Get("/displayNumber/{number}")
+    public void displayNumber(String number) {
+        fourDigitSevenSegmentDisplayHelper.displayNumber(number);
     }
 }
 //end::ex[]
--- pi4micronaut-utils/src/main/java/com/opensourcewithslu/outputdevices/FourDigitSevenSegmentDisplayHelper.java
+++ pi4micronaut-utils/src/main/java/com/opensourcewithslu/outputdevices/FourDigitSevenSegmentDisplayHelper.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.pi4j.io.gpio.digital.DigitalOutput;
 import com.opensourcewithslu.utilities.MultiPinConfiguration;
+import com.opensourcewithslu.outputdevices.SevenSegmentDisplayHelper;
 
 public class FourDigitSevenSegmentDisplayHelper {
     private static Logger log = LoggerFactory.getLogger(LEDHelper.class);
@@ -15,8 +16,13 @@ public class FourDigitSevenSegmentDisplayHelper {
     private final DigitalOutput display3;
     private final DigitalOutput display4;
 
+    private final SevenSegmentDisplayHelper sevenSegmentDisplay1;
+    private final SevenSegmentDisplayHelper sevenSegmentDisplay2;
+    private final SevenSegmentDisplayHelper sevenSegmentDisplay3;
+    private final SevenSegmentDisplayHelper sevenSegmentDisplay4;
+
     //tag::const[]
-    public FourDigitSevenSegmentDisplayHelper(MultiPinConfiguration fourdigitsevenseg)
+    public FourDigitSevenSegmentDisplayHelper(MultiPinConfiguration fourdigitsevenseg, DigitalOutput[] displayList)
     //end::const[]
     {
         DigitalOutput[] displays = (DigitalOutput[]) fourdigitsevenseg.getComponents();
@@ -24,5 +30,47 @@ public class FourDigitSevenSegmentDisplayHelper {
         this.display2 = displays[1];
         this.display3 = displays[2];
         this.display4 = displays[3];
+
+        sevenSegmentDisplay1 = new SevenSegmentDisplayHelper(displayList[0]);
+        sevenSegmentDisplay2 = new SevenSegmentDisplayHelper(displayList[1]);
+        sevenSegmentDisplay3 = new SevenSegmentDisplayHelper(displayList[2]);
+        sevenSegmentDisplay4 = new SevenSegmentDisplayHelper(displayList[3]);
+    }
+
+    //tag::method[]
+    public void displayNumber(String number)
+    //end::method[]
+    {
+        char[] num = number.toCharArray();
+
+        if (num.length > 4) {
+            log.error("Number is too long");
+            return;
+        }
+        if (num.length == 0) {
+            log.error("Number is too short");
+            return;
+        }
+
+        log.info("Displaying number: " + number);
+
+        display2.low();
+        display3.low();
+        display4.low();
+
+        display1.high();
+        sevenSegmentDisplay1.displayNumber(num[0]);
+        if (num.length > 1) {
+            display2.high();
+            sevenSegmentDisplay2.disaplNumber(num[1]);
+        }
+        if (num.length > 2) {
+            display3.high();
+            sevenSegmentDisplay2.disaplNumber(num[2]);
+        }
+        if (num.length > 3) {
+            display4.high();
+            sevenSegmentDisplay2.disaplNumber(num[3]);
+        }
     }
 }
