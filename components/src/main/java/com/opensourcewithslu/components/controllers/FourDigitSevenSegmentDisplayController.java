44efcedf7d57194e1cd388f4a7fa79af13eedfcd
Leandru Martin <leandru.martin@slu.edu>
2024-11-08 18:21:44 -0600

    remove stuff from my attempt 2 do a multi digital output configur8ion

--- components/src/main/java/com/opensourcewithslu/components/controllers/FourDigitSevenSegmentDisplayController.java
+++ components/src/main/java/com/opensourcewithslu/components/controllers/FourDigitSevenSegmentDisplayController.java
@@ -5,7 +5,6 @@ import com.pi4j.context.Context;
 import com.pi4j.io.i2c.I2CConfig;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 //tag::ex[]
@@ -13,7 +12,6 @@ import jakarta.inject.Named;
 public class FourDigitSevenSegmentDisplayController {
     private final FourDigitSevenSegmentDisplayHelper fourDigitSevenSegmentDisplayHelper;
 
-    //    @Inject
     public FourDigitSevenSegmentDisplayController(@Named("four-digit-seven-segment") I2CConfig fourdigsevenseg, Context pi4jContext
     ) {
         this.fourDigitSevenSegmentDisplayHelper = new FourDigitSevenSegmentDisplayHelper(fourdigsevenseg, pi4jContext);
