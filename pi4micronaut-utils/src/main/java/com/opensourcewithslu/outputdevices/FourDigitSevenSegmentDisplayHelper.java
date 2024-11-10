0e8afd73264c2b6f03095829c62a5e49c2f824d6
Leandru Martin <leandru.martin@slu.edu>
2024-11-10 15:21:47 -0600

    Added test and documentation comments to the helper. Added displayValue field and getter.

--- pi4micronaut-utils/src/main/java/com/opensourcewithslu/outputdevices/FourDigitSevenSegmentDisplayHelper.java
+++ pi4micronaut-utils/src/main/java/com/opensourcewithslu/outputdevices/FourDigitSevenSegmentDisplayHelper.java
@@ -7,9 +7,16 @@ import org.slf4j.LoggerFactory;
 import com.pi4j.crowpi.components.SevenSegmentComponent;
 
 public class FourDigitSevenSegmentDisplayHelper {
-    private static final Logger log = LoggerFactory.getLogger(FourDigitSevenSegmentDisplayHelper.class);
+    private static Logger log = LoggerFactory.getLogger(FourDigitSevenSegmentDisplayHelper.class);
     private final SevenSegmentComponent display;
+    private String displayValue;
 
+    /**
+     * Constructs a new FourDigitSevenSegmentDisplayHelper.
+     *
+     * @param i2cConfig   the I2C configuration
+     * @param pi4jContext the Pi4J context
+     */
     //tag::const[]
     public FourDigitSevenSegmentDisplayHelper(I2CConfig i2cConfig, Context pi4jContext)
     //end::const[]
@@ -17,18 +24,31 @@ public class FourDigitSevenSegmentDisplayHelper {
         this.display = new SevenSegmentComponent(pi4jContext, i2cConfig.getBus(), i2cConfig.getDevice());
     }
 
-    public void setEnabled(boolean enabled) {
+    /**
+     * Enables or disables the display.
+     *
+     * @param enabled True to enable, false to disable
+     */
+    //tag::method[]
+    public void setEnabled(boolean enabled)
+    //end::method[]
+    {
         display.setEnabled(enabled);
+        this.displayValue = "";
     }
 
-    public void displayValue(String value) {
-        /* Validate value. Must satisfy following properties:
-            - No more than 4 non-decimal point characters long
-            - No consecutive decimal points
-            - If there are 4 non-decimal point characters, then decimal points must not appear on the ends
-            - Non-decimal point characters must be digits 0 to 1, letters A to F (case-insensitive), -, or space
-         */
-
+    /**
+     * Displays a value on the four-digit seven-segment display.
+     *
+     * @param value The value to display. It can include digits 0-9, letters A-F (case-insensitive),
+     *              hyphens, spaces, and decimal points. The value must not have more than 4 non-decimal
+     *              point characters, no consecutive decimal points, and if there are 4 non-decimal
+     *              point characters, decimal points must not appear on the ends.
+     */
+    //tag::method[]
+    public void displayValue(String value)
+    //end::method[]
+    {
         // Parse out the decimal points
         String noDecimals = value.replaceAll("\\.", "");
 
@@ -51,26 +71,52 @@ public class FourDigitSevenSegmentDisplayHelper {
         }
 
         // Check: Non-decimal point characters must be digits 0 to 1, letters A to F (case-insensitive), -, or space
-        String valid = "1234567890ABCDEF- ";
+        String valid = "1234567890ABCDEFabcdef- ";
         for (char character : noDecimals.toCharArray()) {
             if (valid.indexOf(character) == -1) {
-                log.error("Each display value digit must be numeric, a letter A to F, a hyphen, or a space");
+                log.error("Each display value digit must be numeric, a letter A to F (case insensitive), a hyphen, or a space");
                 return;
             }
         }
 
         display.print(value);
         display.refresh();
+        value = value.toUpperCase();
+        log.info("Displaying value: {}", value);
+        this.displayValue = value;
     }
 
-    public void clear() {
+    /**
+     * Clears the display.
+     */
+    //tag::method[]
+    public void clear()
+    //end::method[]
+    {
         for (int i = 0; i < 4; i++) {
             display.clear();
             display.setDecimalPoint(i, false);
         }
         display.setColon(false);
         display.refresh();
+        this.displayValue = "";
     }
 
+    /**
+     * Sets the logger object.
+     *
+     * @param log Logger object to set the logger to.
+     */
+    public void setLog(Logger log) {
+        FourDigitSevenSegmentDisplayHelper.log = log;
+    }
 
+    /**
+     * Gets the display value.
+     *
+     * @return The display value
+     */
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
--- pi4micronaut-utils/src/test/java/com/opensourcewithslu/outputdevices/FourDigitSevenSegmentDisplayHelperTest.java
+++ pi4micronaut-utils/src/test/java/com/opensourcewithslu/outputdevices/FourDigitSevenSegmentDisplayHelperTest.java
@@ -1,94 +1,272 @@
 package com.opensourcewithslu.outputdevices;
 
-/*import org.junit.jupiter.api.Test;
+import com.pi4j.context.ContextProperties;
+import com.pi4j.io.i2c.I2CConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import org.slf4j.Logger;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.opensourcewithslu.outputdevices.SevenSegmentDisplayHelper;*/
+import com.pi4j.context.Context;
+import com.pi4j.crowpi.components.SevenSegmentComponent;
+
+import java.lang.reflect.Field;
 
 public class FourDigitSevenSegmentDisplayHelperTest {
-    /*FourDigitSevenSegmentDisplayHelper displayHelper = mock(FourDigitSevenSegmentDisplayHelper.class);
-    SevenSegmentDisplayHelper sevenSegmentDisplayHelper = mock(SevenSegmentDisplayHelper.class);
+    I2CConfig i2cConfig = mock(I2CConfig.class);
+    Context pi4jContext = mock(Context.class);
+    ContextProperties contextProperties = mock(ContextProperties.class);
+    FourDigitSevenSegmentDisplayHelper displayHelper;
+    SevenSegmentComponent displayComponent = mock(SevenSegmentComponent.class);
     Logger log = mock(Logger.class);
+
+    @BeforeEach
+    void setUp() throws Exception {
+        // Mock the Context to return a non-null ContextProperties
+        when(pi4jContext.properties()).thenReturn(contextProperties);
+
+        displayHelper = new FourDigitSevenSegmentDisplayHelper(i2cConfig, pi4jContext);
+
+        // Use reflection to set the mock SevenSegmentComponent
+        Field displayField = FourDigitSevenSegmentDisplayHelper.class.getDeclaredField("display");
+        displayField.setAccessible(true);
+        displayField.set(displayHelper, displayComponent);
+    }
+
+    @BeforeEach
+    public void openMocks() {
+        displayHelper.setLog(log);
+    }
 
     @Test
     void longNumberFails() {
-        String number = "12345";
-        displayHelper.displayNumber(number);
-        verify(log).error("Number is too long");
-        verify(log, never()).info("Displaying number: {}", "12345");
-        verify(sevenSegmentDisplayHelper, never()).displayNumber(1);
-        verify(sevenSegmentDisplayHelper, never()).displayNumber(2);
-        verify(sevenSegmentDisplayHelper, never()).displayNumber(3);
-        verify(sevenSegmentDisplayHelper, never()).displayNumber(4);
-        verify(sevenSegmentDisplayHelper, never()).displayNumber(5);
-    }
-
-    void shortNumberFails() {
-        String number = "";
-        displayHelper.displayNumber(number);
-        verify(log).error("Number is too short");
-        verify(log, never()).info("Displaying number: {}", "");
+        String value = "12345";
+        displayHelper.displayValue(value);
+        verify(log).error("Display value must not have more than 4 non-decimal point characters");
+        verify(log, never()).info("Displaying value: {}", "12345");
+        verify(displayComponent, never()).print("12345");
     }
 
     @Test
-    void nonNumberFails() {
-        String number = "abc";
-        displayHelper.displayNumber(number);
-        verify(log).error("Display value must be an integer");
+    void consecutiveDecimalPointsFails() {
+        String value = "1..23";
+        displayHelper.displayValue(value);
+        verify(log).error("Display value cannot have consecutive decimal points");
+        verify(log, never()).info("Displaying value: {}", "1..23");
+        verify(displayComponent, never()).print("1..23");
     }
 
     @Test
-    void negativeNumberFails() {
-        String number = "-1234";
-        displayHelper.displayNumber(number);
-        verify(log).error("Display value must be positive");
+    void decimalPointOnLeftEndFails() {
+        String value = ".1234";
+        displayHelper.displayValue(value);
+        verify(log).error("Display value must have decimal points appearing strictly between the digits");
+        verify(log, never()).info("Displaying value: {}", ".1234");
+        verify(displayComponent, never()).print(".1234");
+    }
+
+    @Test
+    void decimalPointOnRightEndFails() {
+        String value = "1234.";
+        displayHelper.displayValue(value);
+        verify(log).error("Display value must have decimal points appearing strictly between the digits");
+        verify(log, never()).info("Displaying value: {}", "1234.");
+        verify(displayComponent, never()).print("1234.");
+    }
+
+    @Test
+    void decimalPointsOnBothEndsFails() {
+        String value = ".1234.";
+        displayHelper.displayValue(value);
+        verify(log).error("Display value must have decimal points appearing strictly between the digits");
+        verify(log, never()).info("Displaying value: {}", ".1234.");
+        verify(displayComponent, never()).print(".1234.");
+    }
+
+    @Test
+    void invalidCharacterFails() {
+        String value = "G";
+        displayHelper.displayValue(value);
+        verify(log).error("Each display value digit must be numeric, a letter A to F (case insensitive), a hyphen, or a space");
+        verify(log, never()).info("Displaying value: {}", "G");
+        verify(displayComponent, never()).print("G");
+    }
+
+    @Test
+    void displaysLetters() {
+        String value = "ABCD";
+        displayHelper.displayValue(value);
+        verify(log).info("Displaying value: {}", "ABCD");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("ABCD", displayed);
+    }
+
+    @Test
+    void displaysLettersWithLowercase() {
+        String value = "abcd";
+        displayHelper.displayValue(value);
+        verify(log).info("Displaying value: {}", "ABCD");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("ABCD", displayed);
+    }
+
+    @Test
+    void displaysLettersWithMixedCase() {
+        String value = "aBcD";
+        displayHelper.displayValue(value);
+        verify(log).info("Displaying value: {}", "ABCD");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("ABCD", displayed);
+    }
+
+    @Test
+    void displaysHyphen() {
+        String value = "-";
+        displayHelper.displayValue(value);
+        verify(log).info("Displaying value: {}", "-");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("-", displayed);
+    }
+
+    @Test
+    void displaysSpaces() {
+        String value = "2 2";
+        displayHelper.displayValue(value);
+        verify(log).info("Displaying value: {}", "2 2");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("2 2", displayed);
+    }
+
+    @Test
+    void displaysNegativeNumber() {
+        String number = "-123";
+        displayHelper.displayValue(number);
+        verify(log).info("Displaying value: {}", "-123");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("-123", displayed);
     }
 
     @Test
     void displaysFourDigitNumber() {
         String number = "1234";
-        displayHelper.displayNumber(number);
-        verify(displayHelper).displayNumber("1234");
-        verify(log).info("Displaying number: {}", "1234");
+        displayHelper.displayValue(number);
+        verify(log).info("Displaying value: {}", "1234");
 
-//        int displayed = displayHelper.getValue();
-//        assertEquals(1234, displayed);
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("1234", displayed);
     }
 
     @Test
     void displaysThreeDigitNumber() {
         String number = "123";
-        displayHelper.displayNumber(number);
-        verify(displayHelper).displayNumber("123");
-        verify(log).info("Displaying number: {}", "123");
+        displayHelper.displayValue(number);
+        verify(log).info("Displaying value: {}", "123");
 
-//        int displayed = displayHelper.getValue();
-//        assertEquals(123, displayed);
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("123", displayed);
     }
 
     @Test
     void displaysTwoDigitNumber() {
         String number = "34";
-        displayHelper.displayNumber(number);
-        verify(displayHelper).displayNumber("34");
-        verify(log).info("Displaying number: {}", "34");
+        displayHelper.displayValue(number);
+        verify(log).info("Displaying value: {}", "34");
 
-//        int displayed = displayHelper.getValue();
-//        assertEquals(34, displayed);
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("34", displayed);
     }
 
     @Test
     void displaysOneDigitNumber() {
         String number = "4";
-        displayHelper.displayNumber(number);
-        verify(displayHelper).displayNumber("4");
-        verify(log).info("Displaying number: {}", "4");
+        displayHelper.displayValue(number);
+        verify(log).info("Displaying value: {}", "4");
 
-//        int displayed = displayHelper.getValue();
-//        assertEquals(4, displayed);
-    }*/
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("4", displayed);
+    }
+
+    @Test
+    void displaysBlankValue() {
+        String number = "";
+        displayHelper.displayValue(number);
+        verify(log).info("Displaying value: {}", "");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("", displayed);
+    }
+
+    @Test
+    void displaysDecimalNumber() {
+        String number = "1.23";
+        displayHelper.displayValue(number);
+        verify(log).info("Displaying value: {}", "1.23");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("1.23", displayed);
+    }
+
+    @Test
+    void displaysDecimalNumberWithLeadingDecimal() {
+        String number = ".23";
+        displayHelper.displayValue(number);
+        verify(log).info("Displaying value: {}", ".23");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals(".23", displayed);
+    }
+
+    @Test
+    void displaysDecimalNumberWithTrailingDecimal() {
+        String number = "1.";
+        displayHelper.displayValue(number);
+        verify(log).info("Displaying value: {}", "1.");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("1.", displayed);
+    }
+
+    @Test
+    void displaysMultipleDecimals() {
+        String number = "1.2.3.4";
+        displayHelper.displayValue(number);
+        verify(log).info("Displaying value: {}", "1.2.3.4");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("1.2.3.4", displayed);
+    }
+
+    @Test
+    void displaysSpacesAndDecimals() {
+        String number = " . . . ";
+        displayHelper.displayValue(number);
+        verify(log).info("Displaying value: {}", " . . . ");
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals(" . . . ", displayed);
+    }
+
+    @Test
+    void clearDisplay() {
+        String number = "1234";
+        displayHelper.displayValue(number);
+
+        displayHelper.clear();
+        verify(displayComponent, times(4)).clear();
+        verify(displayComponent, times(4)).setDecimalPoint(anyInt(), eq(false));
+        verify(displayComponent).setColon(false);
+        verify(displayComponent, times(2)).refresh();
+
+        String displayed = displayHelper.getDisplayValue();
+        assertEquals("", displayed);
+    }
 }
