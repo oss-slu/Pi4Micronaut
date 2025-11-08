1a46a4d2d32a31d4677752a47554b697b81e23e6
Sandipan <bsandipan99@gmail.com>
2025-11-08 13:23:09 +0530

    Removing ThermistorController old constructor

--- components/src/main/java/com/opensourcewithslu/components/controllers/ThermistorController.java
+++ components/src/main/java/com/opensourcewithslu/components/controllers/ThermistorController.java
@@ -1,25 +1,15 @@
 package com.opensourcewithslu.components.controllers;
 
 import com.opensourcewithslu.inputdevices.ThermistorHelper;
-import com.pi4j.context.Context;
-import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.spi.Spi;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 @Controller("/thermistor")
 public class ThermistorController {
 
     private final ThermistorHelper thermistorHelper;
-
-    // Inject Pi4J Context and digital pins for the thermistor
-    @Inject
-    public ThermistorController(Context pi4jContext) {
-
-        // Initialize the ThermistorHelper with SPI configuration
-        this.thermistorHelper = new ThermistorHelper(pi4jContext);
-    }
 
     /**
      * Constructor for ThermistorController.
